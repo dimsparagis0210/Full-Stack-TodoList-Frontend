ed162a6e226a8fea266e152e011c39e1431428b9
Dimitris Sparagis <dimsparagis@gmail.com>
2025-07-07 16:53:23 +0300

    Feature: Hook for fetching the user

---
+++ src/hooks/home/use-user.tsx
@@ -0,0 +1,15 @@
+import { useQuery } from "@tanstack/react-query";
+import { fetchUserByEmail } from "@/api/user";
+
+export const useUser = (email: string, options: any = {}) => {
+    const { data, isLoading, error } = useQuery({
+        queryKey: ['user', email],
+        queryFn: () => fetchUserByEmail(email), 
+        retry: false,
+        enabled: !!email && options.enabled !== false, 
+        ...options,
+    });
+
+    return { data, isLoading, error };
+}
+
