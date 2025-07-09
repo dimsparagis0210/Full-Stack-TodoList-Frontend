931b081bd48435bd07233487120d9ea6aa8c762c
Dimitris Sparagis <dimsparagis@gmail.com>
2025-07-09 21:27:40 +0300

    Feature: Helper functions for date formatting

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,27 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatDateToMMDD(dateString: string): string {
+  const date = new Date(dateString);
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+
+  return `${day}/${month}`;
+}
+
+export const getInitials = (name: string) => {
+  return name
+    .split(" ")
+    .filter(Boolean)
+    .map(name => name[0])
+    .slice(0, 2)
+    .join("")
+    .toUpperCase();
+}
+
+export const getDateRange = (startDate: string, endDate: string) => {
+  const startDateFormatted = formatDateToMMDD(startDate);
+  const endDateFormatted = formatDateToMMDD(endDate);
+  return `${startDateFormatted} - ${endDateFormatted}`;
+}
