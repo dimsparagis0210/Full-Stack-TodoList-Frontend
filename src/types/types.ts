45b5d8913f467bfe19f1fd70c1588921b6cfe0bc
Dimitris Sparagis <dimsparagis@gmail.com>
2025-07-09 21:27:59 +0300

    Feature: Added and altered types

--- src/types/types.ts
+++ src/types/types.ts
@@ -16,10 +16,18 @@ export type User = {
     accountNonLocked: boolean;
     authorities: string[];
     email: string;
-    id: string;
+    id: number;
     name: string;
     username: string;
     password: string;
+    board: Board;
+    assignedTasks: Task[];
+    createdTasks: Task[];
+}
+
+export type UserDTO = {
+    name: string;
+    email: string;
 }
 
 export type Task = {
@@ -31,7 +39,24 @@ export type Task = {
     startDate: string;
     dueDate: string;
     category: string;
-    createdBy: string;
-    assignedTo: string;
+    createdBy: number;
+    assignedTo: number;
     createdAt: string;
+    boardId: number;
+}
+
+export type TaskDTO = {
+    title: string;
+    description: string;
+    category: string;
+    priority: string;
+    assignedTo: string;
+    startDate: string;
+    dueDate: string;
+    status: string;
+}
+
+export type Board = {
+    id: number;
+    tasks: Task[];
 }
