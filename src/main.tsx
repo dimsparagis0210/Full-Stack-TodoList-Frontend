c23c29e7e45ea79d3e37e3f905da7fdd37330ef0
Dimitris Sparagis <dimsparagis@gmail.com>
2025-07-09 21:23:38 +0300

    Feature: Configured Redux store and added userSlice

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,15 @@ import './index.css'
 import App from './App.tsx'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClient } from './lib/queryClient.ts'
+import { Provider } from 'react-redux'
+import { store } from './store/store.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </QueryClientProvider>
   </StrictMode>,
 )
