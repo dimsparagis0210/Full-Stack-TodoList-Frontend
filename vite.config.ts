fa65dc23a6fa823e64cd38571428f961a2b2155b
Dimitris Sparagis <dimsparagis@gmail.com>
2025-07-10 17:09:50 +0300

    Dockerfile and docker compose

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: 5173,
+    host: true,
+  },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
